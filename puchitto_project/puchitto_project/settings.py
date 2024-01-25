ea5ba746e2e878296c80c310132ad905a02dd602
Sudhirtmg <147568233+Sudhirtmg@users.noreply.github.com>
2024-01-25 11:02:44 +0900

    Update settings.py

--- puchitto_project/puchitto_project/settings.py
+++ puchitto_project/puchitto_project/settings.py
@@ -127,11 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT='/static/'
+STATIC_ROOT='/home/ubuntu/puchitto/static_root/'
 MEDIA_URL='/media/'
-MEDIA_ROOT='/media/'
+MEDIA_ROOT='/home/ubuntu/puchitto/media/'
 STATICFILES_DIRS=[
-os.path.join(BASE_DIR,'static')
+os.path.join(BASE_DIR,'/home/ubuntu/puchitto/static')
 ]
 
 # Default primary key field type
